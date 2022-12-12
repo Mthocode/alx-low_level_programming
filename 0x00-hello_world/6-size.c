c7241e2ad2740b237245c36d6873b1818eed3855
Mthocode <nover22@outlook.com>
2022-12-12 14:36:24 -0800

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,17 @@
 #include<stdio.h>
 /**
  * main - Entry point
- * return: Always 0 (Success)
+ * Discription: give size of of various types on the compute
+ * Return: Always 0 (Success)
  */
 int main()
 {
-	printf("size of a char: %ru byte()\n", sizeof(char));
-	printf("size of a int: %ru byte(s)\n", sizeof(int));
-        printf("size of a long int: /%ru byte(s)\n", sizeof(long int));
-        printf("size of a long long int: %ru byte(s)\n", sizeof(long long int));
-        printf("size of a float: %ru byte(s)\n", sizeof(float));
+	printf("size of a char: %d byte(s)\n", sizeof(char));
+	printf("size of a int: %d byte(s)\n", sizeof(int));
+        printf("size of a long int: %d byte(s)\n", sizeof(long int));
+        printf("size of a long long int: %d byte(s)\n", sizeof(long long int));
+        printf("size of a float: %d byte(s)\n", sizeof(float));
+
         return (0);
 }
 
