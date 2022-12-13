1e5981faf3ed3e7f4725c7bbf6ecf4bfb24e5b94
Mthocode <nover22@outlook.com>
2022-12-13 14:58:00 -0800

    alphabet game

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,21 @@
 #include "main.h"
 /**
  * main - main block
- * Discription: print alpherbet in low case
- * Return: Always 0 (Success)
+ * Discription: print alphabet in low case
+ * Return: Always 0 .
  */
 
 int main(void)
 {
-	char letter = 'a';
+	/*char letter = 'a';
 
 	while (letter < 'z')
 	{
 		_putchar(letter);
 		letter++;
-	}
+	}*/
+
+	print_alphabet(a);
 
 	_putchar('\n');
 
