bb4a2f715b492040349976d5a94d0eb080d4ad76
Mthocode <nover22@outlook.com>
2022-12-13 13:59:54 -0800

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,30 +1,22 @@
-#include <stdio.h>
+#include "main.h"
 /**
  * main - main block
  * Description: display _putchar
- * Return: return 0 (success)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	char unScore = '_';
-	char p = 'p';
-	char u = 'u';
-	char t = 't';
-	char c = 'c';
-	char h = 'h';
-	char a = 'a';
-	char r = 'r';
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
+	_putchar("\n");
 
-	putchar(unScore);
-	putchar(p);
-	putchar(u);
-	putchar(t);
-	putchar(c);
-	putchar(h);
-	putchar(a);
-	putchar(r);
-
-	return 0;
+	return (0);
 }
 
