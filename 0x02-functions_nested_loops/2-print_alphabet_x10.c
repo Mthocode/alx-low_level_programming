7a132ab91415537aff5b6d53c1a5400c128a00f0
Mthocode <nover22@outlook.com>
2022-12-13 16:33:49 -0800

    Alphabet_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,13 +3,13 @@
  * print_alphabet_x10 - Entry point
  * Return: Always 0.
  */
+
 void print_alphabet_x10(void)
 {
 	char letter = 'a';
-	int count = 10;
 	int i = 0;
 
-	while (i <= count)
+	while (i <= 10)
 	{
 		while (letter <= 'z')
 		{
@@ -18,7 +18,6 @@ void print_alphabet_x10(void)
 		}
 
 		_putchar('\n');
-
 		i++;
 	}
 }
