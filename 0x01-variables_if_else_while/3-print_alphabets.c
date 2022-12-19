429e409aa843ecc0b1af77ab29cafe6584dc5c08
Mthocode <nover22@outlook.com>
2022-12-19 06:42:00 -0800

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Discription: print alphabets from lowerc cas to uprt case
+ * Return: Alwayse 0 (success)
+ */
+int main(void)
+{
+	char a = 'a';
+
+	char A = 'A';
+
+	while (a <='z')
+	{
+		putchar(a);
+		a++;
+	}
+	while (A <= 'Z')
+	{
+		putchar(A);
+		A++;
+	}
+	putchar('\n');
+
+	return (0);
+}
