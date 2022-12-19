25b511f7bcad731f1e5ab1456a97c42256940f94
Mthocode <nover22@outlook.com>
2022-12-19 12:46:38 -0800

    print comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Discription: number separated list
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int num;
+
+	while (num < 10)
+	{
+		putchar(num + '0');
+		putchar(',');
+		putchar(' ');
+
+		num++;
+	}
+	putchar('\n');
+
+	return (0);
+}
