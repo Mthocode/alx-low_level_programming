cf9b98806c635d8788e1b4e2e2670a284fb9b6c1
Mthocode <nover22@outlook.com>
2022-12-20 14:42:00 -0800

    absolut value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - absolute value of an integer
+ * @t: value to be evaluated
+ * Return: The absolut value
+ */
+int _abs(int t)
+{
+	if (t >= 0)
+		return (t);
+	else
+		return (-t);
+}
