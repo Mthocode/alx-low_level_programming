241f3ede89a0f776651c964339e0e946cb6e375f
Mthocode <nover22@outlook.com>
2022-12-20 15:03:11 -0800

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_last_digit - prints the last digit of a number
+ * @n: number
+ * Return: value of the last digit
+ */
+int print_last_digit(int n)
+{
+	int ldigit = n % 10;
+
+	if (ldigit < 0)
+		ldigit *= -1;
+
+	_putchar(ldigit + '0');
+
+	return (0);
+}
