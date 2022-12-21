a286bf90260ba8c32eb4d429701f6c6450b8e487
Mthocode <nover22@outlook.com>
2022-12-20 16:05:00 -0800

    jack bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of the
+ * day of Jack Bauer, starting from 00:00 to 23:59
+ */
+void jack_bauer(void)
+{
+	int hours;
+	int min;
+
+	hours = 0;
+	min = 0;
+
+	while (hours <= 23)
+	{
+		while (min <= 59)
+		{
+			_putchar((hours / 10) + '0');
+			_putchar((hours % 10) + '0');
+			_putchar(':');
+			_putchar((min / 10) + '0');
+			_putchar((min % 10) + '0');
+
+			_putchar('\n');
+
+			min++;
+		}
+
+		_putchar('\n');
+
+		hours++;
+	}
+}
