7c64577612493158d90a9f35a8419ae114cf4db2
Mthocode <nover22@outlook.com>
2022-12-22 07:42:37 -0800

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add -  adds two integers and returns the result
+ * @a: firs integer to be add
+ * @b: second integer to be add
+ * Return: returns result
+ */
+int add(int a, int b)
+{
+	int result = a = b;
+
+	return (result);
+}
