186d9dca02e6894dcb4b7eb6d7543fb9a2548393
Mthocode <nover22@outlook.com>
2023-01-07 13:00:09 -0800

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isupper - checks for uppercase character
+ * @c: character to be tested
+ * Return: returns 1 if c is uppercase or 0 otherwise
+ */
+int _isupper(int c)
+{
+	if (c == C)
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
