fd7a75d5b1a24e7162a893092074a061bb879153
Mthocode <nover22@outlook.com>
2023-01-11 14:05:20 -0800

    rev_string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * rev_string - reverses a string
+ * @s: string
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	char rv = s[0];
+	int c = 0;
+	int i;
+
+	while (s[c] != '\0')
+		c++;
+	for (i = 0; i < c; i++)
+		_putchar(s[c-1]);
+	_putchar('\n');
+}
