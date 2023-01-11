85759f7d145de5a87c55884d42d120282afa772e
Mthocode <nover22@outlook.com>
2023-01-11 15:28:29 -0800

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * *_strcpy - copies the string pointed to by src, including the terminating null
+ * @dest: first string
+ * @src: second string
+ * Return: char
+ */ 
+char *_strcpy(char *dest, char *src)
+{
+	int l = 0;
+
+	while (*(src + l) != '0')
+	{
+		*(dest + l) = *(src + l);
+		l++;
+	}
+
+	*(dest + l) = '\0';
+	return(dest);
+}
