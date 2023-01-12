6cce2c1a4d4b9de4d62bfc10dc4e9ba38b8e571a
Mthocode <nover22@outlook.com>
2023-01-12 15:22:27 -0800

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * *_strncat - concatenates two strings
+ * @dest: first string
+ * @src: second string
+ * @n: integes
+ * Return: char
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int a, b;
+
+	a = 0;
+	while (dest[a] != '\0')
+	{
+		a++;
+	}
+	b = 0;
+	while (b < n && src[b] != '\0')
+	{
+		dest[i] = src[j];
+		a++;
+		b++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
