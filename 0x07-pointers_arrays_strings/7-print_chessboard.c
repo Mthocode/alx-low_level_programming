cc3f67421fafc9a1f18d4d5687a31a42e7a4da68
Mthocode <nover22@outlook.com>
2023-01-20 03:01:05 -0800

    Chess is mental torture

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -9,7 +9,7 @@ void print_chessboard(char (*a)[8])
 	int count1;
 	int count2;
 
-	for (count1 = 0; a[count1][7]; count1)
+	for (count1 = 0; a[count1][7]; count1++)
 	{
 		for (count2 = 0; count2 < 8; count2++)
 			_putchar(a[count1][count2]);
