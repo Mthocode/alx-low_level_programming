180e09f9db6cacbd8b61111d0da53d748dd2ca7f
Mthocode <nover22@outlook.com>
2023-01-20 02:00:36 -0800

    strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -3,7 +3,7 @@
  * _strpbrk - searches a string for any of a set of bytes
  * @s: String
  * @accept: String to look for
- * Return: pointer to the byte in s that 
+ * Return: pointer to the byte in s that
  * matches one of the bytes in accept, or NULL
  */
 char *_strpbrk(char *s, char *accept)
